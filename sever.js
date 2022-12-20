ff8c3d66ac06685a74a0cdc2d61fdc66ba130889
RayTsai <45555194+Rayray4509@users.noreply.github.com>
2022-12-20 15:01:21 +0800

    test

--- sever.js
+++ sever.js
@@ -1,14 +1,14 @@
 import express from "express";
-import session from "./config/session.js";
-import passport from "./config/passport.js";
-import apiRouters from "./routers/apiRouters.js";
-import adminRouters from "./routers/adminRouters.js";
-import memberRouter from "./routers/memberRouter.js";
-import aurth from "./controllers/aurth.js";
+// import session from "./config/session.js";
+// import passport from "./config/passport.js";
+// import apiRouters from "./routers/apiRouters.js";
+// import adminRouters from "./routers/adminRouters.js";
+// import memberRouter from "./routers/memberRouter.js";
+// import aurth from "./controllers/aurth.js";
 import frontRouter from "./routers/front.js"
-import cors from "cors"
-import https from "https"
-import fs from "fs"
+// import cors from "cors"
+// import https from "https"
+// import fs from "fs"
 const app = express()
 // const sever = https.createServer({
 //     key: fs.readFileSync("server.key"),
@@ -24,14 +24,14 @@ const app = express()
 
 app.use(express.json());
 app.use(express.static('./dist'));
-app.use(session.sessionSet);
-app.use(passport.passport.initialize());
-app.use(passport.passport.session());
+// app.use(session.sessionSet);
+// app.use(passport.passport.initialize());
+// app.use(passport.passport.session());
 
 app.use("/",frontRouter.router)
-app.use("/api",apiRouters.router)
-app.use("/admin",aurth.adminAurth,adminRouters.router)
-app.use("/member",aurth.memberAurth,memberRouter.router)
+// app.use("/api",apiRouters.router)
+// app.use("/admin",aurth.adminAurth,adminRouters.router)
+// app.use("/member",aurth.memberAurth,memberRouter.router)
 
 
 app.listen("3000",()=>{
